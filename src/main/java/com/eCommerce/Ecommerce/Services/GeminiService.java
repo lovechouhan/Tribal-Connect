b9df434dd9e576cd494a043507217a9f978326c2
Love Chouhan <149556567+lovechouhan@users.noreply.github.com>
2026-05-22 13:24:27 +0530

    Refactor GeminiService to use configuration properties
    Removed hardcoded API key and model from GeminiService.

--- src/main/java/com/eCommerce/Ecommerce/Services/GeminiService.java
+++ src/main/java/com/eCommerce/Ecommerce/Services/GeminiService.java
@@ -15,8 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Deprecated
 public class GeminiService {
 
-    // private final String API_KEY = "AIzaSyBXZ2wPw_5GWwg-_USwOhbLlSX0dzZWke8"; // ⚠️ apni key lagao
-    // private final String MODEL = "gemini-2.5-flash"; // ya gemini-1.5-flash
+    
 
     @Value("${google.api.key}")
     private String API_KEY;
